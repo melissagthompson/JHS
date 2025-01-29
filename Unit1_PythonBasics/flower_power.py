6ece0be5f0c82616ab10c8ac2ddc5c7e25d5198c
melissagthompson <51178551+melissagthompson@users.noreply.github.com>
2025-01-28 21:21:43 -0600

    Update flower_power.py

--- Unit1_PythonBasics/flower_power.py
+++ Unit1_PythonBasics/flower_power.py
@@ -4,3 +4,12 @@
 
 import turtle
 t = turtle.Turtle()
+screen = turtle.Screen()
+
+screen.bgcolor('lightblue')
+
+# a function that can draw a petal
+def draw_petal():
+    for _ in range(2):
+        t.circle(50, 60)  # Adjust petal size
+        t.left(120)
